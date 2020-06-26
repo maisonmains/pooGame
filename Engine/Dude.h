021143d88a322d41c3825171ce0f21e44d7e72e8
Leslie Maina <lesliemaina12@gmail.com>
2020-06-26 09:06:11 +0300

    Dude&Poo-float changes

--- Engine/Dude.h
+++ Engine/Dude.h
@@ -9,14 +9,14 @@ public:
 	void ClampToScreen();
 	void Draw( Graphics& gfx ) const;
 	void Update( const Keyboard& kbd );
-	int GetX() const;
-	int GetY() const;
+	float GetX() const;
+	float GetY() const;
 	int GetWidth() const;
 	int GetHeight() const;
 private:
-	int x = 400;
-	int y = 300;
-	static constexpr int speed = 1;
+	float x = 400;
+	float y = 300;
+	static constexpr float speed = 1;
 	static constexpr int width = 20;
 	static constexpr int height = 20;
 };
--- Engine/Poo.h
+++ Engine/Poo.h
@@ -6,15 +6,15 @@
 class Poo
 {
 public:
-	void Init( int in_x,int in_y,int in_vx,int in_vy );
+	void Init( float in_x, float in_y, float in_vx, float in_vy );
 	void Update();
 	bool TestCollision( const Dude& dude ) const;
 	void Draw( Graphics& gfx ) const;
 private:
-	int x;
-	int y;
-	int vx;
-	int vy;
+	float x;
+	float y;
+	float vx;
+	float vy;
 	static constexpr int width = 24;
 	static constexpr int height = 24;
 	bool initialized = false;
